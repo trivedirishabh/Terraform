47c5b69b2cbe36754bb07f5375ce0fc40ce08c8f
Rishabh Trivedi <68752602+rishabh-trivedi23@users.noreply.github.com>
2021-10-14 15:33:47 +0200

    Update create_adf.tf

--- adf/create_adf.tf
+++ adf/create_adf.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "example" {
-  name     = "rg-kb799"
+  name     = "rg-adf-kb799"
   location = "West Europe"
 }
 
@@ -7,4 +7,11 @@ resource "azurerm_data_factory" "example" {
   name                = "adf-kb799"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  github_configuration {
+      account_name = "rishabh-trivedi23"
+      branch_name = "main"
+      git_url = "https://github.com"
+      repository_name = "Terraform"
+      root_folder = "/"
+  }
 }
